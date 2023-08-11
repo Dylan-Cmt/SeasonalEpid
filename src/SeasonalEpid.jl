2410a714de80c03b81eeae7849522d25cc9ec2a9
Dylan-Cmt <cormet.dylan@gmail.com>
2023-08-11 11:17:26 +0200

    correct export

--- src/SeasonalEpid.jl
+++ src/SeasonalEpid.jl
@@ -1,7 +1,11 @@
 "This is a dummy module to illustrate documentation creation"
 module SeasonalEpid
 
-export A
+export TimeParam
+
+export Param, Elaborate1Strain, Elaborate2Strains, Compact1Strain, Compact2Strains, ParamSoilborneCompact1Strain, ParamAirborneElaborate1Strain
+export StateParam0, StateCompact, StateElaborate
+
 export greet
 include("structs.jl")
 include("functions.jl")
