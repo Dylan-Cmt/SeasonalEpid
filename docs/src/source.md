3530959294931bba7104c70a45a22c188934531e
Dylan-Cmt <cormet.dylan@gmail.com>
2023-08-11 11:12:55 +0200

    add more struct

--- docs/src/source.md
+++ docs/src/source.md
@@ -11,12 +11,44 @@ Depth = 3
 
 > **_NOTE:_**  If you want to know more about what a struct contains, type `fieldnames(structname)`.
 
+### Time parameters
+
 ```@docs
-A
+TimeParam
 ```
+
+### Biological parameters
+
+These structs can be called biological parameters but note that they contain also some informations about the model, like if it is elaborate, and the number of states.
+
+```@docs
+Param
+Elaborate1Strain
+Elaborate2Strains
+Compact1Strain
+Compact2Strains
+ParamSoilborneCompact1Strain
+ParamAirborneElaborate1Strain
+```
+
+### States
+
+```@docs
+StateParam0
+StateCompact
+StateElaborate
+```
+
+## SeasonalEpid functions
+
+### Functions in `functions.jl`
 
 ```@autodocs
 Modules = [SeasonalEpid]
 Order   = [:function]
 Pages   = ["functions.jl"]
 ```
+
+## Reproducing figures
+
+Maybe we can show here few examples.
--- src/structs.jl
+++ src/structs.jl
@@ -1,6 +1,130 @@
+using Parameters, StaticArrays
+
 """
-It's just a random struct.
+Stock all time variables.
 """
-struct A
-    a
+@with_kw struct TimeParam
+    T::Float64 = 365
+    @assert T > 0
+    @assert T <= 365 # year length
+    τ::Float64 = 184
+    @assert τ <= T # crop season length
+    Δt::Float64 = 0.1
+    @assert Δt > 0 # step
+    tspang::Tuple{Float64,Float64} = (0, τ) # tspan for growing
+    tspanw::Tuple{Float64,Float64} = (τ, T) # tspan for winter	
+end
+
+"""
+This is the most global param. Subtypes of `Param` are also abstract type.
+"""
+abstract type Param end
+
+"""
+This param is also a global param but it is only for elaborate 1 strain models.
+"""
+abstract type Elaborate1Strain <: Param end
+
+"""
+This param is also a global param but it is only for elaborate 2 strain2 models.
+"""
+abstract type Elaborate2Strains <: Param end
+
+"""
+This param is also a global param but it is only for compact 1 strain models.
+"""
+abstract type Compact1Strain <: Param end
+
+"""
+This param is also a global param but it is only for compact 2 strains models.
+"""
+abstract type Compact2Strains <: Param end
+
+"""
+This param is very specific and it is built to construct  soilborne compact 1 strain models.
+"""
+@with_kw struct ParamSoilborneCompact1Strain{T<:Float64} <: Compact1Strain
+    # growing season parameters
+    α::T = 0.024
+    @assert α > 0 # Infected host plants removal rate
+    β::T = 0.04875
+    @assert β > 0 # Secondary infection rate
+
+    # convertion into primary inoculum parameter
+    θ::T = 0.04875
+    @assert θ > 0 # Primary infection rate
+    Π::T = 1.0
+    @assert Π > 0 # Conversion rate from I to P (at the end of the season)
+    μ::T = 0.0072
+    @assert μ > 0
+    λ::T = 0.0072
+    @assert λ > 0
+    n::T = 1.0
+    @assert n >= 0 # Initial plant density
+
+    # type of model and state number 
+    isElaborate = false
+    statelength = 2
+end
+
+"""
+This param is very specific and it is built to construct  airborne elaborate 1 strain models.
+"""
+@with_kw struct ParamAirborneElaborate1Strain{T<:Float64} <: Elaborate1Strain
+    # growing season parameters
+    Λ::T = 0.052
+    @assert Λ > 0 # Primary inoculum density independent depletion rate
+    Θ::T = 0.04875
+    @assert Θ > 0 # Primary infection rate
+    α::T = 0.024
+    @assert α > 0 # Infected host plants removal rate
+    β::T = 0.04875
+    @assert β > 0 # Secondary infection rate
+
+    # convertion into primary inoculum parameter
+    Π::T = 1.0
+    @assert Π > 0 # Conversion rate from I to P (at the end of the season)
+
+    # winter-specific mortality parameter
+    μ::T = 0.0072
+    @assert μ > 0 # Winter season mortality rate of primary inoculum
+
+    # new susceptible host plant density parameter 
+    n::T = 1.0
+    @assert n >= 0 # Initial plant density
+
+    # type of model and state number 
+    isElaborate = true
+    statelength = 3
+end
+
+"""
+This is the most global Stateparam. Subtypes of `StateParam0` are also abstract type.
+"""
+abstract type StateParam0 end
+
+"""
+This stateparam is very specific to compact 1 strain models and it stocks model's states at the beginning of a season.
+"""
+@with_kw struct StateCompact <: StateParam0
+    S0::Float64 = 0.99
+    @assert S0 >= 0
+    I0::Float64 = 0.01
+    @assert I0 >= 0
+    @assert S0 + I0 <= 1
+    State0 = @SVector [S0, I0]
+end
+
+"""
+This stateparam is very specific to elaborate 1 strain models and it stocks model's states at the beginning of a season.
+"""
+@with_kw struct StateElaborate <: StateParam0
+    P0::Float64 = 0.01
+    @assert P0 >= 0
+    S0::Float64 = 0.99
+    @assert S0 >= 0
+    I0::Float64 = 0.00
+    @assert I0 >= 0
+    @assert S0 + I0 <= 1
+    State0 = @SVector [P0, S0, I0]
 end
