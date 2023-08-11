f9c54a472997c89eb92d9939aa82b2231c37b9f1
Dylan-Cmt <cormet.dylan@gmail.com>
2023-08-11 10:55:10 +0200

    add struts.jl

--- src/SeasonalEpid.jl
+++ src/SeasonalEpid.jl
@@ -1,7 +1,9 @@
 "This is a dummy module to illustrate documentation creation"
 module SeasonalEpid
 
+export A
 export greet
+include("structs.jl")
 include("functions.jl")
 
 end # module SeasonalEpid
---
+++ src/structs.jl
@@ -0,0 +1,6 @@
+"""
+It's just a random struct.
+"""
+struct A
+    a
+end
